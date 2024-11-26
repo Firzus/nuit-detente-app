df39050f3f1307f9b8f303ac9a674e04e3d2d0cf
Firzus <92395888+Firzus@users.noreply.github.com>
2024-11-26 22:47:25 +0100

    Add initial components, configuration, and styles; remove unused files

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="container sticky top-0 flex h-16 items-center justify-between">
+      <h3>Nuit Détente</h3>
+    </header>
+  )
+}
